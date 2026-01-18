ad6ff79a5633dd6aeac168e0ea5a326e7f6bc17b
Jason Whitaker <jason@thewhitakers.ca>
2026-01-18 07:08:20 -0500

    check and exit or force install if dir exists

--- .dotfiles/install.sh
+++ .dotfiles/install.sh
@@ -2,18 +2,48 @@
 
 export DOTFILES_HOME=$HOME/.dotfiles
 
-if [ -e "$DOTFILES_HOME" ]; then
+FORCE_INSTALL=${FORCE_INSTALL:-false}
+
+install_dotfiles() {
+  echo "Installing dotfiles to $DOTFILES_HOME"
+
+  git clone --bare https://github.com/jwhitaker/dotfiles.git $HOME/.dotfiles
+
+  function dotfiles {
+    /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
+  }
+
+  dotfiles restore --staged .
+  dotfiles restore .
+
+  dotfiles config status.showUntrackedFiles no
+}
+
+backup_existing_dotfiles() {
   backupFilename="$DOTFILES_HOME.$(date +%F).backup"
   echo "Backing up existing $DOTFILES_HOME to $backupFilename"
   mv "$DOTFILES_HOME" "$backupFilename"
-fi
+} 
 
-git clone --bare https://github.com/jwhitaker/dotfiles.git $HOME/.dotfiles
-function dotfiles {
-   /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
+check_for_dotfiles() {
+  echo "Checking for existing dotfiles in $DOTFILES_HOME"
+
+  if [ -e "$DOTFILES_HOME" ]; then
+    echo "Dotfiles already installed in $DOTFILES_HOME"
+    echo "If you want to reinstall, set FORCE_INSTALL=true and run this script again."
+    exit 1
+  else
+    echo "No existing dotfiles found in $DOTFILES_HOME"
+  fi
 }
 
-dotfiles restore --staged .
-dotfiles restore .
+if [ "$FORCE_INSTALL" = true ]; then
+  if [ -e "$DOTFILES_HOME" ]; then
+    backup_existing_dotfiles
+  fi
+else
+  check_for_dotfiles
+fi
 
-dotfiles config status.showUntrackedFiles no
+install_dotfiles
+
